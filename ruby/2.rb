be47f27fcb42efcac4bdf0906ab087650c3eae89
Slavkata <slavkirilov00@gmail.com>
2016-12-11 16:01:13 +0200

    Update 2.rb

--- ruby/2.rb
+++ ruby/2.rb
@@ -1,13 +1,19 @@
+require 'csv'
+require 'json'
 
+result = 0
+json = JSON.parse(File.read(ARGV[1]))
+csv = File.open(ARGV[0])
 
-Develop a program named FirstName_LastName_ClassNumber_aa5f54.rb
-
-1. you are given two command line arguments;
-1.1 if there are other arguments they should be discarded;
-1.2 The first argument is the full path of a CSV file with 4 columns
-1.3 The second argument is the full path of a JSON file
-
-2. Find the sum of all the values in the JSON file where:
- the key of this value is exactly equal to a value in any row on column 2 from the CSV
- 
-3. Print only the result value
+json.each_key { |key|
+	help = 0
+	CSV.foreach(csv) do |row|
+		if key.to_i == row[1].to_i then
+			help = 1
+		end
+	end
+	if help > 0 then
+			result += json.fetch(key).to_i
+		end
+}
+puts result

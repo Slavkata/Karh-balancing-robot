10b816514763199a9ebe082a6bac3d5522ea56d3
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:36:25 +0200

    Update 23.rb

--- ruby/23.rb
+++ ruby/23.rb
@@ -1,1 +1,12 @@
 
+Develop a program named FirstName_LastName_ClassNumber_f8a509.rb
+
+1. you are given two command line arguments;
+1.1 if there are other arguments they should be discarded;
+1.2 The first argument is the full path of a CSV file with 4 columns
+1.3 The second argument is the full path of a JSON file
+
+2. Find the min of all the values in the JSON file where:
+ the key of this value is contained in the value of any row on column 2 from the CSV
+ 
+3. Print only the result value

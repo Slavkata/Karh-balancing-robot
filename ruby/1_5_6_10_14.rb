f5ab6b5614c1626ce949164542eb5ba16425d191
Slavkata <slavkirilov00@gmail.com>
2016-12-11 14:13:39 +0200

    Update 1_5_6_10_14.rb

--- ruby/1_5_6_10_14.rb
+++ ruby/1_5_6_10_14.rb
@@ -1,9 +1,18 @@
-Develop a program named FirstName_LastName_ClassNumber_d1c589.rb
+require 'crack'
+require 'json'
 
-1. you are given two arguments;
-1.1 if there are other arguments they should be discarded;
-1.2 The first argument is to an XML file
-1.3 The second argument is to a JSON file
-2. Find the number of nodes in the xml that have a name equal to a key in the JSON file;
-3. Calculate calculate the square of this number;
-4. Print only the result value
+result = 0
+
+json = JSON.parse(File.read(ARGV[0]))
+xml = Crack::XML.parse(File.read(ARGV[1]))
+xml = xml.fetch(xml.keys[0]).keys
+
+xml.each { |node|
+	json.each_key { |key|
+		if node == key then
+			result += 1
+		end
+	}
+}
+
+puts result

68434444c5d023e58335eb0b2f51b9d491a36d87
Slavkata <slavkirilov00@gmail.com>
2016-12-11 13:56:58 +0200

    Update 11_12_16_17_22_27.rb

--- ruby/11_12_16_17_22_27.rb
+++ ruby/11_12_16_17_22_27.rb
@@ -1,14 +1,20 @@
 
-begin=
-Develop a program named FirstName_LastName_ClassNumber_8ac981.rb
+require 'csv'
+require 'json'
+result = 1
+json = JSON.parse(File.read(ARGV[1]))
+csv = (ARGV[0])
+json.each_key{ |key|
+	i = 0
+	help = 0
+	CSV.foreach(csv) do |row|
+		if key != row[2] then
+			help += 1
+		end
+		if help == json.keys.count then
+			result *= json.fetch(key).to_i
+		end
+end
+}
 
-1. you are given two command line arguments;
-1.1 if there are other arguments they should be discarded;
-1.2 The first argument is the full path of a CSV file with 4 columns
-1.3 The second argument is the full path of a JSON file
-
-2. Find the product of all the values in the JSON file where:
- the key of this value is not equal to a value in any row on column 2 from the CSV
- 
-3. Print only the result value
-=end  
+puts result

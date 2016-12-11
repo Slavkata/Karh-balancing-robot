3ca67742f982d2941a1ac878d5bd404fc35b4d4c
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:35:34 +0200

    Update 1_5_6_10_14.rb

--- ruby/1_5_6_10_14.rb
+++ ruby/1_5_6_10_14.rb
@@ -1,2 +1,9 @@
-require 'json'
-require 'crack'
+Develop a program named FirstName_LastName_ClassNumber_d1c589.rb
+
+1. you are given two arguments;
+1.1 if there are other arguments they should be discarded;
+1.2 The first argument is to an XML file
+1.3 The second argument is to a JSON file
+2. Find the number of nodes in the xml that have a name equal to a key in the JSON file;
+3. Calculate calculate the square of this number;
+4. Print only the result value

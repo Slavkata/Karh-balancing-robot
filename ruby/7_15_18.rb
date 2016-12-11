3a5ecc50ccb8403334b9982b25168c35ec875340
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:36:46 +0200

    Update 7_15_18.rb

--- ruby/7_15_18.rb
+++ ruby/7_15_18.rb
@@ -1,1 +1,18 @@
 
+
+Develop a program named FirstName_LastName_ClassNumber_fc52e8.rb
+
+1. you are given an argument that is the full path to CSV file
+1.1 if there are other arguments they should be discarded
+2. The CSV file has four columns
+3. The first row of the CSV are the headers with the following names
+
+		C1, C2, C3, C4
+
+4. Find the sum of all the values for column C3 
+	where 
+	C1 > 17
+	and 
+	C2 is not a digit
+
+5. Print only the result value

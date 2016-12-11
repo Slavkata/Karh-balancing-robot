faca3badad1d8c095402b87fa4da237705302b6e
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:34:52 +0200

    Update 11_12_16_17_22_27.rb

--- ruby/11_12_16_17_22_27.rb
+++ ruby/11_12_16_17_22_27.rb
@@ -1,1 +1,14 @@
 
+begin=
+Develop a program named FirstName_LastName_ClassNumber_8ac981.rb
+
+1. you are given two command line arguments;
+1.1 if there are other arguments they should be discarded;
+1.2 The first argument is the full path of a CSV file with 4 columns
+1.3 The second argument is the full path of a JSON file
+
+2. Find the product of all the values in the JSON file where:
+ the key of this value is not equal to a value in any row on column 2 from the CSV
+ 
+3. Print only the result value
+=end  

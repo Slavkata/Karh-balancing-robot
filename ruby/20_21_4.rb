1586fd30e8bf7b3aafa844f42371732f60ce7a8b
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:36:09 +0200

    Update 20_21_4.rb

--- ruby/20_21_4.rb
+++ ruby/20_21_4.rb
@@ -1,1 +1,18 @@
 
+
+Develop a program named FirstName_LastName_ClassNumber_8de28f.rb
+
+1. you are given an argument that is the full path to CSV file
+1.1 if there are other arguments they should be discarded
+2. The CSV file has four columns
+3. The first row of the CSV are the headers with the following names
+
+		C1, C2, C3, C4
+
+4. Find the product of all the values for column C3 
+	where 
+	C1 < C4
+	and 
+	C3 = C4+3
+
+5. Print only the result value

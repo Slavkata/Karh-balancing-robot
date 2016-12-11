9689a0bb05ef1c62445f501898505e93be3d4210
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:35:53 +0200

    Update 2.rb

--- ruby/2.rb
+++ ruby/2.rb
@@ -1,1 +1,13 @@
 
+
+Develop a program named FirstName_LastName_ClassNumber_aa5f54.rb
+
+1. you are given two command line arguments;
+1.1 if there are other arguments they should be discarded;
+1.2 The first argument is the full path of a CSV file with 4 columns
+1.3 The second argument is the full path of a JSON file
+
+2. Find the sum of all the values in the JSON file where:
+ the key of this value is exactly equal to a value in any row on column 2 from the CSV
+ 
+3. Print only the result value

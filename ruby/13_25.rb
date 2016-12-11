1a564eae68b36dd810cd3c457fd5536b77ec9ed0
Slavkata <slavkirilov00@gmail.com>
2016-12-11 11:35:15 +0200

    Update 13_25.rb

--- ruby/13_25.rb
+++ ruby/13_25.rb
@@ -1,1 +1,17 @@
 
+Develop a program named FirstName_LastName_ClassNumber_194ab3.rb
+
+1. you are given an argument that is the full path to CSV file
+1.1 if there are other arguments they should be discarded
+2. The CSV file has four columns
+3. The first row of the CSV are the headers with the following names
+
+		C1, C2, C3, C4
+
+4. Find the sum of all the values for column C3 
+	where 
+	C1 > 17
+	and 
+	C2 contais the string 'tues'
+
+5. Print only the result value
